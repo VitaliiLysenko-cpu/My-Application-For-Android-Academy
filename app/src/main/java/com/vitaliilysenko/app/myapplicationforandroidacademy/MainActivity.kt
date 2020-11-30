3a3cc44a0ae6634e4f31ce27f301f16a3ef55a4f
Vitalii <vit47903@gmail.com>
2020-11-30 16:10:16 +0200

    homework4

--- app/src/main/java/com/vitaliilysenko/app/myapplicationforandroidacademy/MainActivity.kt
+++ app/src/main/java/com/vitaliilysenko/app/myapplicationforandroidacademy/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.vitaliilysenko.app.myapplicationforandroidacademy
 
-import android.content.Intent
+import android.app.ActionBar
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnChangeFragmentClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val frame = FrameLayout(this)
+        frame.id = R.id.container_fragment
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.first_activity_text_view)
-        textView.setOnClickListener { moveToNextScreen() }
-
-    }
-    private fun moveToNextScreen(){
-      val intent = Intent(this, MovieDetailsActivity::class.java)
-        startActivity(intent)
+        setContentView(frame, ActionBar.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT))
+        if (savedInstanceState == null) {
+            addFirstFragment()
+        }
     }
 
+    private fun addFirstFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container_fragment, FragmentMoviesList())
+                .commit()
+    }
+
+    override fun addSecondFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .addToBackStack("Back")
+                .add(R.id.container_fragment, FragmentMoviesDetails())
+                .commit()
+    }
 }
